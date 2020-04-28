c3ba3efb9f2afa98fcccf1f544f1e1485d552442
michalkb <wloch.it@gmail.com>
2020-04-28 23:12:38 +0200

    cleaning up, created python fixer for data

---
+++ Python_Cleaner/Clean_data.py
@@ -0,0 +1,18 @@
+
+
+
+
+if __name__ == "main":
+    # read the data from clean_test
+
+    # read the data from train
+
+    # read the data from the whoel dataset.
+
+    # create matrices which contain the values separatered by ;
+
+    # join survival rate from whole dataset to the test
+
+    # cocatenate train with test.
+
+    # save as new clean_complete.

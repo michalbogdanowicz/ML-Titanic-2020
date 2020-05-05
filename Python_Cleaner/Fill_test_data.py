27876f162e06485a9ad9dc8958f50b59d00ba2ab
michalkb <wloch.it@gmail.com>
2020-05-05 13:48:31 +0200

    data ready

--- Python_Cleaner/Fill_test_data.py
+++ Python_Cleaner/Fill_test_data.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     # read the data from test_clean
     test_data = read_file('test_clean.csv')
     print(test_data)
-    print('zjokan')
     # read the data from the whole dataset.
     all_data = read_file('titanic.csv')
     print(all_data)

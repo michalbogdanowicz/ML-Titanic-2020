adaa9ea3a183f1d0e10f0af07e49cfe1eae8370f
michalkb <wloch.it@gmail.com>
2020-05-07 23:12:07 +0200

    logistic regression updated to use the new data. Removed 2 old columns left

--- Logistic_Regression/logTitanic.m
+++ Logistic_Regression/logTitanic.m
@@ -7,7 +7,8 @@ data = load('titanic_numerical_clean.csv');
 
 
 %==========================================================
- y = data(:, 11);
+num_columns = columns(data)
+ y = data(:, num_columns);
  c = cvpartition(y, 'KFold', 10)
 
 mean_accuracy = 0;
@@ -19,11 +20,11 @@ for iteration = 1:10 # k = 10
   train_data = data(train_l_vector, :);
   test_data = data(test_l_data, :);
 
-  train_X = train_data(:, 1:10);
-  train_y = train_data(:, 11);
+  train_X = train_data(:, 1:num_columns-1);
+  train_y = train_data(:, num_columns);
   
   m = length(train_y);
-  t_init=zeros(11,1);
+  t_init=zeros(num_columns,1);
 
   train_X = [ones(m,1) train_X]; %this should be the bias.
   [cost, grad] = costFunction(t_init, train_X,train_y);
@@ -44,10 +45,10 @@ for iteration = 1:10 # k = 10
   %%gradient
   
   % need to predict on test now with a bias.
-  test_y = test_data(:, 11);
+  test_y = test_data(:, num_columns);
 
   m = length(test_y);
-  test_X = test_data(:, 1:10);
+  test_X = test_data(:, 1:num_columns-1);
   test_X = [ones(m,1) test_X]; %this should be the bias.
 
   
